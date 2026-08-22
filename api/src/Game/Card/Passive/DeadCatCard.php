4bc5341395691217904ddb32b885b0e28235d32c
Fan2Shrek <pierre27.ambroise@gmail.com>
2026-08-22 11:27:26 +0200

    [API] Add dead cat card

---
+++ api/src/Game/Card/Passive/DeadCatCard.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Game\Card\Passive;
+
+use App\Enum\CardRarityEnum;
+use App\Enum\CardSetEnum;
+use App\Enum\GameEventTypeEnum;
+use App\Game\Card\CardState;
+use App\Game\Card\Interface\DeathAwareInterface;
+use App\Game\Card\Trait\DeathAwareTrait;
+use App\Game\GameContext;
+use App\Game\GameUtils;
+
+final class DeadCatCard extends AbstractPassiveCard implements DeathAwareInterface
+{
+    use DeathAwareTrait;
+
+    private const int REVIVE_HP = 1;
+
+    private int $counter = 9;
+
+    public function getId(): string
+    {
+        return 'DeadCat';
+    }
+
+    public function getDescription(): string
+    {
+        return GameUtils::formatDescription(parent::getDescription(), ['value' => $this->getValue(self::REVIVE_HP), 'value2' => $this->counter]);
+    }
+
+    public function onPlayerDeath(GameContext $gameContext, string $deadPlayerId): void
+    {
+        if ($deadPlayerId !== $this->getOwnerId()) {
+            return;
+        }
+
+        $player = $gameContext->getPlayerStateById($this->getOwnerId());
+
+        $gameContext->heal(abs(min(0, $player->healthPoints)) + self::REVIVE_HP, $this->getOwnerId());
+
+        $gameContext->pushGameEvent(GameEventTypeEnum::CARD_STATE_UPDATED, [
+            'cardId' => $this->getInstanceId(),
+            'playerId' => $this->getOwnerId(),
+            'stateToUpdate' => [
+                'counter' => --$this->counter,
+            ],
+        ]);
+
+        if ($this->counter <= 0) {
+            $gameContext->discardCard($this->getInstanceId());
+        }
+    }
+
+    public function setState(CardState $state): void
+    {
+        parent::setState($state);
+
+        $this->counter = $state->values['counter'] ?? 9;
+    }
+
+    public function getSerie(): CardSetEnum
+    {
+        return CardSetEnum::TBOI;
+    }
+
+    public function getRarity(): CardRarityEnum
+    {
+        return CardRarityEnum::RARE;
+    }
+}
---
+++ api/tests/Unit/Game/Card/Passive/DeadCatCardTest.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Game\Card\Passive;
+
+use App\Enum\GameEventTypeEnum;
+use App\Game\Card\CardState;
+use App\Game\Card\Passive\DeadCatCard;
+use App\Game\GameContext;
+use App\Tests\Unit\Game\Card\CardTestCase;
+
+final class DeadCatCardTest extends CardTestCase
+{
+    protected function getCardFQCN(): string
+    {
+        return DeadCatCard::class;
+    }
+
+    public function testOnPlayerDeathDoesNothingWhenOtherPlayerDies(): void
+    {
+        $card = $this->getCard();
+        $ctx = $this->createGameContext();
+
+        $card->onPlayerDeath($ctx, '2');
+
+        self::assertCount(0, $ctx->flushEvents());
+    }
+
+    public function testOnPlayerDeathHealsAndDecreasesCounter(): void
+    {
+        $card = $this->getCard();
+        $state = $this->createGameContext()->state;
+        $ctx = new GameContext($state->withUpdatedPlayer($state->player1->withUpdatedHealth(0)), '1');
+
+        $card->onPlayerDeath($ctx, '1');
+        $events = $ctx->flushEvents();
+
+        self::assertCount(2, $events);
+
+        self::assertSame(GameEventTypeEnum::HEAL_APPLIED, $events[0]->type);
+        self::assertSame('1', $events[0]->data['targetId']);
+        self::assertSame(1, $events[0]->data['amount']);
+
+        self::assertSame(GameEventTypeEnum::CARD_STATE_UPDATED, $events[1]->type);
+        self::assertSame('test_card', $events[1]->data['cardId']);
+        self::assertSame('1', $events[1]->data['playerId']);
+        self::assertSame(8, $events[1]->data['stateToUpdate']['counter']);
+    }
+
+    public function testOnPlayerDeathHealsMissingHealthPointsWhenNegative(): void
+    {
+        $card = $this->getCard();
+        $state = $this->createGameContext()->state;
+        $ctx = new GameContext($state->withUpdatedPlayer($state->player1->withUpdatedHealth(-3)), '1');
+
+        $card->onPlayerDeath($ctx, '1');
+        $events = $ctx->flushEvents();
+
+        self::assertSame(4, $events[0]->data['amount']);
+    }
+
+    public function testOnPlayerDeathDiscardsCardWhenCounterReachesZero(): void
+    {
+        $card = $this->getCard();
+        $card->setState(new CardState('test_card', $card->getId(), '1', values: ['counter' => 1]));
+
+        $state = $this->createGameContext()->state;
+        $ctx = new GameContext($state->withUpdatedPlayer($state->player1->withUpdatedHealth(0)), '1');
+
+        $card->onPlayerDeath($ctx, '1');
+        $events = $ctx->flushEvents();
+
+        self::assertCount(3, $events);
+        self::assertSame(0, $events[1]->data['stateToUpdate']['counter']);
+        self::assertSame(GameEventTypeEnum::CARD_DISCARDED, $events[2]->type);
+        self::assertSame('test_card', $events[2]->data['cardId']);
+    }
+
+    public function testOnPlayerDeathDoesNotDiscardCardWhenCounterAboveZero(): void
+    {
+        $card = $this->getCard();
+        $card->setState(new CardState('test_card', $card->getId(), '1', values: ['counter' => 2]));
+
+        $state = $this->createGameContext()->state;
+        $ctx = new GameContext($state->withUpdatedPlayer($state->player1->withUpdatedHealth(0)), '1');
+
+        $card->onPlayerDeath($ctx, '1');
+        $events = $ctx->flushEvents();
+
+        self::assertCount(2, $events);
+    }
+}
